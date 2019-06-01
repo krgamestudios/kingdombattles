3b69c4f5e1cdba7c899451902c96a2fb4309157e
Kayne Ruse <kayneruse@gmail.com>
2019-06-01 17:09:14 +1000

    Fixed some issues

--- src/components/panels/equipment.jsx
+++ src/components/panels/equipment.jsx
@@ -17,8 +17,10 @@ class Equipment extends React.Component {
 	}
 
 	render() {
+		let display = this.flattenStructure(this.state, this.props.scientists);
+
 		//if there are no scientists
-		if (this.props.scientists <= 0) {
+		if (this.props.scientists <= 0 && display.length === 0) {
 			return (
 				<div className='panel'>
 					<p className='centered'>You have no scientists!</p>
@@ -26,8 +28,6 @@ class Equipment extends React.Component {
 				</div>
 			);
 		}
-
-		let display = this.flattenStructure(this.state, this.props.scientists);
 
 		return (
 			<div className='panel'>
@@ -91,13 +91,13 @@ class Equipment extends React.Component {
 
 		Object.keys(structure.statistics).map((type) => {
 			Object.keys(structure.statistics[type]).map((name) => {
-				//don't render high level items
-				if (structure.statistics[type][name].scientistsRequired > scientists) {
+				//don't render high level items you don't own
+				if (structure.statistics[type][name].scientistsRequired > scientists && !structure.owned[name]) {
 					return;
 				}
 
-				//if you can't buy it and you down own it, don't render it (for legendary items)
-				if (!structure.statistics[type][name].purchasable && !structure.owned[name]) {
+				//if you can't see it and you don't own it, don't render it (for legendary items)
+				if (!structure.statistics[type][name].visible && !structure.owned[name]) {
 					return;
 				}
 
@@ -106,7 +106,8 @@ class Equipment extends React.Component {
 					name: name,
 					type: type,
 					owned: (structure.owned && structure.owned[name]) || 0,
-					...structure.statistics[type][name]
+					...structure.statistics[type][name],
+					purchasable: structure.statistics[type][name].purchasable && structure.statistics[type][name].scientistsRequired <= scientists //negate purchasing of too-high-level items
 				});
 			});
 		});
