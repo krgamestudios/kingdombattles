dd19aa97a3b0daf87c94e14a55e145453f0f3586
Kayne Ruse <kayneruse@gmail.com>
2019-06-07 02:51:25 +1000

    Added activity field to diagnostics

--- server/diagnostics.js
+++ server/diagnostics.js
@@ -9,7 +9,7 @@ let { log } = require('../common/utilities.js');
 
 const runDailyDiagnostics = (connection) => {
 	let dailyJob = new CronJob('0 0 0 * * *', () => {
-		let query = 'INSERT INTO diagnostics (playerCount, returnedPlayerCount, totalGold, totalRecruitments, totalDeaths, totalCombats) VALUES ((SELECT COUNT(*) FROM profiles), (SELECT COUNT(*) FROM profiles WHERE (recruits + soldiers + spies + scientists) >= 2), (SELECT SUM(gold) FROM profiles), (IFNULL((SELECT SUM(quantity) FROM diagnosticsEvents WHERE eventName = "recruit"), 0)), (IFNULL((SELECT SUM(quantity) FROM diagnosticsEvents WHERE eventName = "death"), 0)), (SELECT COUNT(*) FROM pastCombat WHERE eventTime >= DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY)));';
+		let query = 'INSERT INTO diagnostics (playerCount, returnedPlayerCount, totalGold, totalRecruitments, totalDeaths, totalCombats, activity) VALUES ((SELECT COUNT(*) FROM profiles), (SELECT COUNT(*) FROM profiles WHERE (recruits + soldiers + spies + scientists) >= 2), (SELECT SUM(gold) FROM profiles), (IFNULL((SELECT SUM(quantity) FROM diagnosticsEvents WHERE eventName = "recruit"), 0)), (IFNULL((SELECT SUM(quantity) FROM diagnosticsEvents WHERE eventName = "death"), 0)), (SELECT COUNT(*) FROM pastCombat WHERE eventTime >= DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY)), (SELECT COUNT(*) FROM accounts WHERE lastActivityTime >= DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY)));';
 		connection.query(query, (err) => {
 			if (err) throw err;
 
--- sql/update.sql
+++ sql/update.sql
@@ -1,3 +1,3 @@
 #NOTE: ALWAYS, ALWAYS, ALWAYS write a script in revert.sql that undoes these changes
 
-ALTER TABLE accounts ADD COLUMN lastActivityTime TIMESTAMP DEFAULT '2019-01-01 00:00:00';
+ALTER TABLE diagnostics ADD COLUMN activity INTEGER DEFAULT NULL;
